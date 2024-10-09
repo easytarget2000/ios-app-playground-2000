ff7080831763868a52f69b92bee60113b5c6e987
Michel Sievers <mitch@easy-target.eu>
2024-10-09 22:47:17 +0200

    feat: prepare ViewModel

--- MobileAppPlayground/ContentView.swift
+++ MobileAppPlayground/ContentView.swift
@@ -8,17 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var viewModel: ContentViewModel
+    
+    init(viewModel: ContentViewModel) {
+        self.viewModel = viewModel
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(viewModel.copy)
+            Button(viewModel.buttonTitle) {
+                viewModel.onButtonPress()
+            }
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(viewModel: PreviewContentViewModel())
+}
+
+protocol ContentViewModel {
+    var copy: String { get }
+    var buttonTitle: String { get }
+    func onButtonPress()
+}
+
+@Observable final class PreviewContentViewModel: ContentViewModel {
+    let copy: String = "Lorem ipsum. (Preview)"
+    let buttonTitle: String = "Perform something. (Preview)"
+    
+    func onButtonPress() {
+        // No-op
+    }
+    
+    init() {
+        NSLog("PreviewContentViewModel initialized.")
+    }
 }
